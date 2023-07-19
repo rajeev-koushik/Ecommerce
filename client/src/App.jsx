d4d521d354ede56c85fef3f351f17f97b6877fb5
rajeev-koushik <rajeevkoushikyg@gmail.com>
2023-07-19 19:53:38 +0530

    Navbar ready

---
+++ client/src/App.jsx
@@ -0,0 +1,46 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { Home } from "./pages/Home/Home";
+import { Shop } from "./pages/Shop/Shop";
+import { Product } from "./pages/Product/Product";
+import { Navbar } from "./components/Navbar/Navbar";
+import "./App.scss";
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/shop/:id",
+        element: <Shop />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import "./Navbar.scss";
+
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left">
+          <div className="item">
+            <Link className="link" to="/">
+              Home
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/shop/:id">
+              Shop
+            </Link>
+          </div>
+        </div>
+        <div className="right">
+          <div className="icons">
+            <SearchOutlinedIcon />
+            <div className="cartIcon">
+              <ShoppingCartOutlinedIcon />
+              <span>0</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
