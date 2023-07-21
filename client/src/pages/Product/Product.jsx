a9d0c92d3f9451c8c042abb29531cb67d7bca01a
rajeev-koushik <rajeevkoushikyg@gmail.com>
2023-07-21 13:20:10 +0530

    Fixed async bug

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -9,44 +9,59 @@ export const Product = () => {
   const value = useParams().id;
   const id = parseInt(value);
   console.log("id ", id);
+  const [loading, setLoading] = useState(true);
   const [selectedImg, setSelectedImg] = useState(0);
   const [records, setRecords] = useState([]);
 
   useEffect(() => {
-    fetch(`http://127.0.0.1:8000/product/${id}`)
-      .then((res) => res.json())
-      .then((data) => setRecords(data))
-      .catch((err) => console.log(err));
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(`http://127.0.0.1:8000/product/${id}`);
+        const data = await res.json();
+        setRecords(data);
+        setLoading(false);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    fetchData();
   }, []);
 
   console.log("records ", records);
 
   return (
     <div className="product">
-      <div className="left">
-        <div className="images">
-          <img src={records?.images[0]} onClick={(e) => setSelectedImg(0)} />
-          <img src={records?.images[1]} onClick={(e) => setSelectedImg(1)} />
-        </div>
-        <div className="mainImg">
-          <img src={records?.images[selectedImg]} />
-        </div>
-      </div>
-      <div className="right">
-        <h1>{records?.title}</h1>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis
-          vitae quod dicta repudiandae? Quaerat deleniti velit accusantium
-          suscipit et, consectetur blanditiis, quia dignissimos illum nulla
-          quisquam similique illo fuga iste!
-        </p>
-        <span>Price per unit</span>
-        <div className="add">
-          <h2>${records?.price}</h2>
-          <button>Buy Now</button>
-          <AddShoppingCartOutlinedIcon />
-        </div>
-      </div>
+      {loading ? (
+        console.log("Loading...")
+      ) : (
+        <>
+          <div className="left">
+            <div className="images">
+              <img src={records.images[0]} onClick={(e) => setSelectedImg(0)} />
+              <img src={records.images[1]} onClick={(e) => setSelectedImg(1)} />
+            </div>
+            <div className="mainImg">
+              <img src={records.images[selectedImg]} />
+            </div>
+          </div>
+          <div className="right">
+            <h1>{records.title}</h1>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Reiciendis vitae quod dicta repudiandae? Quaerat deleniti velit
+              accusantium suscipit et, consectetur blanditiis, quia dignissimos
+              illum nulla quisquam similique illo fuga iste!
+            </p>
+            <span>Price per unit</span>
+            <div className="add">
+              <h2>${records.price}</h2>
+              <button>Buy Now</button>
+              <AddShoppingCartOutlinedIcon />
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
